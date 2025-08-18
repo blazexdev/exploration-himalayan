4a72fb218d614071df45c5f68983aa5e508d3771
Blaze <blazexdev@gmail.com>
2025-08-18 16:51:36 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://exploration-himalayan-server.onrender.com' });
+const API = axios.create({ baseURL: 'https://exploration-himalayan-server.onrender.com:1000)' });
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
