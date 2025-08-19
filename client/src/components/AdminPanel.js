9ad639ce2eafd6e851cb9eda65c246b117024c38
Blaze <blazexdev@gmail.com>
2025-08-19 19:16:16 +0530

    Update AdminPanel.js

--- client/src/components/AdminPanel.js
+++ client/src/components/AdminPanel.js
@@ -9,7 +9,7 @@ const icons = {
   edit: <svg xmlns="[http://www.w3.org/2000/svg](http://www.w3.org/2000/svg)" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>,
   send: <svg xmlns="[http://www.w3.org/2000/svg](http://www.w3.org/2000/svg)" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>,
 };
-const LOGO_URL = "[https://i.ibb.co/VW3kNJGd/1000006623-removebg-preview.png](https://i.ibb.co/VW3kNJGd/1000006623-removebg-preview.png)";
+const LOGO_URL = "https://i.ibb.co/VW3kNJGd/1000006623-removebg-preview.png](https://i.ibb.co/VW3kNJGd/1000006623-removebg-preview.png";
 const ITEMS_PER_PAGE = 5;
 
 const AdminPanel = ({ 
