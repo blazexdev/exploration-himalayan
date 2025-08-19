240b653a06ca376af47a88fb3bd6d98112ecd0f4
Blaze <blazexdev@gmail.com>
2025-08-19 19:37:18 +0530

    Update messageController.js

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,6 @@
 const Message = require('../models/Message');
+const Notification = require('../models/Notification'); // Import Notification model
+const User = require('../models/User');
 
 exports.getMessages = async (req, res) => {
     try {
@@ -13,6 +15,25 @@ exports.sendMessage = async (req, res) => {
     try {
         const newMessage = new Message(req.body);
         const message = await newMessage.save();
+
+        // Create notification for the recipient
+        if (message.from === 'admin') {
+            const user = await User.findOne({ email: message.to });
+            if (user) {
+                await Notification.create({
+                    recipient: user._id.toString(),
+                    message: `You have a new message from Admin.`,
+                    link: '/contact'
+                });
+            }
+        } else {
+            await Notification.create({
+                recipient: 'admin',
+                message: `You have a new message from ${message.from}.`,
+                link: '/admin'
+            });
+        }
+
         res.json(message);
     } catch (err) {
         res.status(500).send('Server error');
